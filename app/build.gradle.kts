ab65d515370589ebd626594b83f6647a5ed78891
Rostyslav <megarostyslav@gmail.com>
2025-06-11 23:57:38 +0300

    Fixed all things and add additonal activities in manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,8 @@ dependencies {
     implementation(libs.androidx.swiperefreshlayout)
     implementation(libs.firebase.functions.ktx)
     implementation(libs.firebase.messaging.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
